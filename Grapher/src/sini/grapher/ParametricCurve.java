41395e830a4faf1e741ac290ac2f4e24413c8321
Sini <hinds13245@gmail.com>
2021-07-04 15:45:26 +0100

    wrote generateMesh method for ParametricCurve

--- Grapher/src/sini/grapher/ParametricCurve.java
+++ Grapher/src/sini/grapher/ParametricCurve.java
@@ -1,13 +1,26 @@
 package sini.grapher;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ParametricCurve {
+	
 	private Function f;
 	
 	public ParametricCurve(Function f) {
 		this.f = f;
 	}
 	
-	public void generatePoints(Interval interval) {
+	public ArrayList<double[][]> generateMesh(Interval interval, double step) {
+		ArrayList<double[][]> mesh = new ArrayList<double[][]>();
+		Iterator<double[][]> iter = interval.getPointCubeIterator(step);
 		
+		while(iter.hasNext()) {
+			double[][] currentMeshSeg = iter.next();
+			for(double[] point: currentMeshSeg) point = f.image(point);
+			mesh.add(currentMeshSeg);
+		}
+		
+		return mesh;
 	}
 }
